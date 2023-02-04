6d2f7946e07f2bcf5c32af9557714470cb5873b1
binyamin.regev <beniregev@gmail.com>
2023-02-05 00:10:28 +0200

    Refactoring - fixing syntax/typo

--- src/main/java/com/beniregev/kafka/java17temurin_springboot_kafka_consumer_app/listener/KafkaConsumer.java
+++ src/main/java/com/beniregev/kafka/java17temurin_springboot_kafka_consumer_app/listener/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.beniregev.kafka.java17temurin_springboot_kafka_consumer_app.listener;
 
+import com.beniregev.kafka.java17temurin_springboot_kafka_consumer_app.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +8,16 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
 
     @KafkaListener(topics = "kafka-app-topic", groupId = "myGroup")
-    public void comsume(String message) {
+    public void consume(String message) {
         System.out.println(String.format("Consumed message \"%s\"", message));
+
+    }
+
+    @KafkaListener(topics = "kafka-app-json-topic",
+            groupId = "myGroupJson",
+            containerFactory = "userKafkaListenerFactory")
+    public void consume(User user) {
+        System.out.println("Consumed message " + user);
 
     }
 }
